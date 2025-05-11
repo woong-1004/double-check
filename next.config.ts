e400eb91cde55d4e599114c99112685000eb8292
woong-1004 <worldiswoong@gmail.com>
2025-05-11 17:08:18 +0900

    Fix webpack cache error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { dev }) => {
+    if (dev) {
+      // Disable cache in development to avoid ENOENT errors
+      config.cache = false;
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
